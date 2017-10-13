205a6fb9cc4c2fb9344027717374c9d655af3806
Defjam <defjam9007@gmail.com>
2017-10-13 11:37:42 +0200

    edit

--- client_reboot.sh
+++ client_reboot.sh
@@ -5,35 +5,55 @@
 #     EMAIL:                             helpi9007@gmail.com
 #     CREATED:                      2017-09-12
 #
-#     MODIFIED BY:                macbook
-#     MODIFIED DATE:           2017-09-12
+#     MODIFIED BY:                Defjam
+#     MODIFIED DATE:           2017-10-13
 #
 #     DESCRIPTION:               "Client herunterfahren oder restarten. Log mit Datum und IP erstellen"
 #
 #     VERSION:                        1.0
 #======================================== ==================================
-
-# Ueberprüfung ob $1 und $2 gegeben sind
+##########################################
+## Ueberprüfung ob $1 und $2 gegeben sind
+##########################################
 
 if [ -z "$1" -o -z "$2" ] ;then
 echo "So geht das nicht"
 exit 1;
 fi
 
-#Prüfen, ob Host erreichbar
+############################################
+##    Datum und Client IP in Log schreiben
+############################################
+
+cat <<-EOF  >> /var/log/openhab2/restart.log 2>&1
+
+#######################################
+##    Datum : $(date +"%m-%d-%Y")
+##    Client 192.168.2.$1
+#######################################
+
+EOF
+
+#######################################
+##    Prüfen, ob Host erreichbar
+#######################################
 
 ping -c 1 192.168.2.$1 &> /dev/null
 if [ "$?" != 0 ] ; then
-        echo "Offline"
+        echo "Offline" >> /var/log/openhab2/restart.log 2>&1
         exit 1;
 fi
 
-# Datum und Client IP in Log schreiben
-
-echo "########$(date +"%m-%d-%Y")########" >> /var/log/openhab2/restart.log 2>&1
-echo "reboot von Client 192.168.2.$1" >> /var/log/openhab2/restart.log 2>&1
-
 # Reboot durchführen und Details in Log Schreiben
-/usr/bin/ssh -i /scripts/.ssh/id_rsa root@192.168.2.$1 "shutdown $2 now" >> /var/log/openhab2/restart.log 2>&1
-
+echo "Reboot wird gestartet" >> /var/log/openhab2/restart.log 2>&1
+sleep 5
+echo "" >> /var/log/openhab2/restart.log 2>&1
+if [[ "$1" = 8 ]]; then
+    /usr/bin/ssh -i /scripts/.ssh/id_rsa root@192.168.2.$1 "/sbin/_shutdown $2 now" >> /var/log/openhab2/restart.log 2>&1
+else
+    /usr/bin/ssh -i /scripts/.ssh/id_rsa root@192.168.2.$1 "shutdown $2 now" >> /var/log/openhab2/restart.log 2>&1
+fi
+echo "" >> /var/log/openhab2/restart.log 2>&1
 echo ""  >> /var/log/openhab2/restart.log 2>&1
+
+echo "#######################################"  >> /var/log/openhab2/restart.log 2>&1
