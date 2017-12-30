cf04c92181d06c720f527bdda0ea22cd81740299
MacBooK <defjam9007@gmail.com>
2017-12-30 02:14:12 +0100

    add

---
+++ Rig /hash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Pruefen, ob Suffix angegeben
+if [ -z "$1" ] ;then
+    echo "So geht das nicht"
+    exit 1;
+fi
+
+#Prüfen, ob Host erreichbar
+ping -c 1 192.168.2.$1 &> /dev/null
+if [ "$?" != 0 ] ; then
+        echo "Offline"
+        exit 1;
+fi
+
+# Wert des Sensors auslesen
+INPUT=$(/usr/bin/ssh -i /scripts/.ssh/id_rsa root@192.168.2.$1 "show stats | grep hash:| tr -s ' '| cut -d ' ' -f 2 ")
+# Wert ausgeben
+echo $INPUT | cut -d ' ' -f 2
